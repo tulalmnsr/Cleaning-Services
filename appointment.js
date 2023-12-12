16864d654eb78e19b689f19885a3ec6770b705a4
zeinaberjawi43 <117642379+zeinaberjawi43@users.noreply.github.com>
2023-12-12 21:45:43 +0200

    Update appointment.js

--- appointment.js
+++ appointment.js
@@ -1,49 +1,184 @@
-const body=document.querySelector("body"),
-    siderbar=body.querySelector(".sidebar"),
-    toggle=body.querySelector(".toggle"),
-    searchBtn=body.querySelector(".search-box"),
-    modeText=body.querySelector(".mode-text"),
-    modeSwitch=body.querySelector(".toggle-switch");
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
-    toggle.addEventListener("click", ()=>{
-        siderbar.classList.toggle("close");
-    }
-    );
-    searchBtn.addEventListener("click", ()=>{
-        siderbar.classList.remove("close");
-    }
-    );
+allSideMenu.forEach((item) => {
+  const li = item.parentElement;
 
-
-    modeSwitch.addEventListener("click", ()=>{
-        body.classList.toggle("dark");
-
-        if(body.classList.contains("dark")){
-            modeText.innerText="light Mode";
-        }else{
-            modeText.innerText="Dark Mode";
-        }
-    }
-    );
-
-    document.getElementById('textInput').addEventListener('input', function (event) {
-        const searchTerm = event.target.value.toLowerCase();
-        const listItems = document.querySelectorAll('.atable tr td');
-    
-        listItems.forEach(function (item) {
-            const itemText = item.textContent.toLowerCase();
-    
-            if (itemText.includes(searchTerm)) {
-                item.style.display = 'list-item';
-            } else {
-                item.style.display = 'none';
-            }
-        });
+  item.addEventListener('click', function () {
+    allSideMenu.forEach((i) => {
+      i.parentElement.classList.remove('active');
     });
-    function deleteRow(i) {
-        // Get the reference of the button's parent row
-        var row = i.parentNode.parentNode;
+    li.classList.add('active');
+  });
+});
 
-        // Delete the row
-        row.parentNode.removeChild(row);
+//Date Set up
+
+const date = (document.getElementById('date').innerHTML =
+  new Date().getFullYear());
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+  sidebar.classList.toggle('hide');
+});
+
+const searchButton = document.querySelector(
+  '#content nav form .form-input button'
+)
+const searchButtonIcon = document.querySelector(
+  '#content nav form .form-input button .bx'
+)
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault();
+    searchForm.classList.toggle('show');
+    if (searchForm.classList.contains('show')) {
+      searchButtonIcon.classList.replace('bx-search', 'bx-x');
+    } else {
+      searchButtonIcon.classList.replace('bx-x', 'bx-search');
     }
+  }
+})
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add('hide');
+} else if (window.innerWidth > 576) {
+  searchButtonIcon.classList.replace('bx-x', 'bx-search');
+  searchForm.classList.remove('show');
+}
+
+window.addEventListener('resize', function () {
+  if (this.innerWidth > 576) {
+    searchButtonIcon.classList.replace('bx-x', 'bx-search');
+    searchForm.classList.remove('show');
+  }
+})
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+  if (this.checked) {
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+  }
+});
+
+
+
+//script.js 
+const cardsPerPage = 5; // Number of cards to show per page 
+const dataContainer = document.getElementById('data-container'); 
+const pagination = document.getElementById('pagination'); 
+const prevButton = document.getElementById('prev'); 
+const nextButton = document.getElementById('next'); 
+const pageNumbers = document.getElementById('page-numbers'); 
+const pageLinks = document.querySelectorAll('.page-link'); 
+
+const cards = 
+	Array.from(dataContainer.getElementsByTagName('tr')); 
+
+// Calculate the total number of pages 
+const totalPages = Math.ceil(cards.length / cardsPerPage); 
+let currentPage = 1; 
+
+// Function to display cards for a specific page 
+function displayPage(page) { 
+	const startIndex = (page - 1) * cardsPerPage; 
+	const endIndex = startIndex + cardsPerPage; 
+	cards.forEach((card, index) => { 
+		if (index >= startIndex && index < endIndex) { 
+			card.style.display = ''; 
+		} else { 
+			card.style.display = 'none'; 
+		} 
+	}); 
+} 
+
+// Function to update pagination buttons and page numbers 
+function updatePagination() { 
+	pageNumbers.textContent = 
+		`Page ${currentPage} of ${totalPages}`; 
+	prevButton.disabled = currentPage === 1; 
+	nextButton.disabled = currentPage === totalPages; 
+	pageLinks.forEach((link) => { 
+		const page = parseInt(link.getAttribute('data-page')); 
+		link.classList.toggle('active', page === currentPage); 
+	}); 
+} 
+
+// Event listener for "Previous" button 
+prevButton.addEventListener('click', () => { 
+	if (currentPage > 1) { 
+		currentPage--; 
+		displayPage(currentPage); 
+		updatePagination(); 
+	} 
+}); 
+
+// Event listener for "Next" button 
+nextButton.addEventListener('click', () => { 
+	if (currentPage < totalPages) { 
+		currentPage++; 
+		displayPage(currentPage); 
+		updatePagination(); 
+	} 
+}); 
+
+// Event listener for page number buttons 
+pageLinks.forEach((link) => { 
+	link.addEventListener('click', (e) => { 
+		e.preventDefault(); 
+		const page = parseInt(link.getAttribute('data-page')); 
+		if (page !== currentPage) { 
+			currentPage = page; 
+			displayPage(currentPage); 
+			updatePagination(); 
+		} 
+	}); 
+}); 
+
+// Initial page load 
+displayPage(currentPage); 
+updatePagination();
+
+
+       // get selected row
+            // display selected row data in text input
+            
+            var table = document.getElementById("table"),rIndex;
+            
+            for(var i = 1; i < table.rows.length; i++)
+            {
+                table.rows[i].onclick = function()
+                {
+                    rIndex = this.rowIndex;
+                    console.log(rIndex);
+                    
+                    document.getElementById("fname").value = this.cells[0].innerHTML;
+                    document.getElementById("address").value = this.cells[1].innerHTML;
+                    document.getElementById("date").value = this.cells[2].innerHTML;
+                    document.getElementById("time").value = this.cells[3].innerHTML;
+                    document.getElementById("type").value = this.cells[4].innerHTML;
+                };
+            }
+            function update() {
+              var select = document.getElementById('services-option');
+              var option = select.options[select.selectedIndex];
+      
+              table.rows[rIndex].cells[4].value=option.value;
+            }
+              // edit the row
+              function editRow()
+              {
+                
+                  table.rows[rIndex].cells[0].innerHTML = document.getElementById("name").value;
+                  table.rows[rIndex].cells[1].innerHTML = document.getElementById("date").value;
+                  table.rows[rIndex].cells[2].innerHTML = document.getElementById("time").value;
+                  table.rows[rIndex].cells[3].innerHTML = document.getElementById("address").value;
+                  update();
+              }
