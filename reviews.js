558608b3806541eaca717342fa322ef2e08a06de
Batoul El Mansour <elmansourbatoul@gmail.com>
2023-12-14 12:10:12 +0200

    Update reviews.js

--- reviews.js
+++ reviews.js
@@ -108,6 +108,50 @@ document.addEventListener('DOMContentLoaded', function () {
     sidebar.classList.toggle('active');
   });
 });
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu')
+// ...
+const otherSidebar = document.getElementById('sidebar');
+// ...
+
+
+menuBar.addEventListener('click', function () {
+  sidebar.classList.toggle('hide')
+})
+
+const searchButton = document.querySelector(
+  '#content nav form .form-input button'
+)
+const searchButtonIcon = document.querySelector(
+  '#content nav form .form-input button .bx'
+)
+const searchForm = document.querySelector('#content nav form')
+
+searchButton.addEventListener('click', function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault()
+    searchForm.classList.toggle('show')
+    if (searchForm.classList.contains('show')) {
+      searchButtonIcon.classList.replace('bx-search', 'bx-x')
+    } else {
+      searchButtonIcon.classList.replace('bx-x', 'bx-search')
+    }
+  }
+})
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add('hide')
+} else if (window.innerWidth > 576) {
+  searchButtonIcon.classList.replace('bx-x', 'bx-search')
+  searchForm.classList.remove('show')
+}
+
+window.addEventListener('resize', function () {
+  if (this.innerWidth > 576) {
+    searchButtonIcon.classList.replace('bx-x', 'bx-search')
+    searchForm.classList.remove('show')
+  }
+})
 
   
 
